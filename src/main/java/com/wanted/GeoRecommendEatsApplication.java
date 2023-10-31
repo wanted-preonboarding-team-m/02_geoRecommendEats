b0e8dfe5e2ac554e6d76ea0e2a4fcc484e45a180
choisoyeong <thdud3410@gmail.com>
2023-10-31 18:29:42 +0900

    chore: 프로젝트 구조 변경

--- src/main/java/com/wanted/GeoRecommendEatsApplication.java
+++ src/main/java/com/wanted/GeoRecommendEatsApplication.java
@@ -1,4 +1,4 @@
-package com.wanted._geoRecommendEats;
+package com.wanted;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
