9bb7f526d2929078dbbd71b4ea9c1b1e921d72ae
choisoyeong <thdud3410@gmail.com>
2023-10-31 17:35:50 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/wanted/_geoRecommendEats/GeoRecommendEatsApplication.java
@@ -0,0 +1,13 @@
+package com.wanted._geoRecommendEats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoRecommendEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoRecommendEatsApplication.class, args);
+	}
+
+}
