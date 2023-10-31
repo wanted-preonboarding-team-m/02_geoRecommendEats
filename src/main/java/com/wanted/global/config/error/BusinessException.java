a8c94fae90cecde137a1ef5c16f3e78e736040f6
choisoyeong <thdud3410@gmail.com>
2023-10-31 19:00:02 +0900

    feat: Error 형식 구현

---
+++ src/main/java/com/wanted/global/config/error/BusinessException.java
@@ -0,0 +1,35 @@
+package com.wanted.global.config.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 로직에서 예외를 발생시킬 때 사용하는 언체크 예외
+ */
+@Getter
+public class BusinessException extends RuntimeException {
+
+  //오류 발생 부분의 값. 명확하게 없으면 Null.
+  private final String invalidValue;
+  //오류 필드명.
+  private final String fieldName;
+  //오류 상태 코드.
+  private final HttpStatus httpStatus;
+  //오류 메시지
+  private final String message;
+
+  /**
+   * 비지니스 에러를 ErroCode Enum으로 생성
+   *
+   * @param invalidValue 오류 발생 부분의 값
+   * @param fieldName    오류 필드 명
+   * @param errorCode    오류 상태코드와 메시지가 담긴 Enum
+   */
+  public BusinessException(Object invalidValue, String fieldName, ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.invalidValue = invalidValue != null ? invalidValue.toString() : null;
+    this.fieldName = fieldName;
+    this.httpStatus = errorCode.getHttpStatus();
+    this.message = errorCode.getMessage();
+  }
+}
