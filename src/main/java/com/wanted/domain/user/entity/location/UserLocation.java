aa0c688b35ee437d24388b942afaa488fb92ad52
kawksung <alstjd5479@naver.com>
2023-11-03 12:24:47 +0900

    feat: 유저 위치 Entity 및 DAO 생성

---
+++ src/main/java/com/wanted/domain/user/entity/location/UserLocation.java
@@ -0,0 +1,36 @@
+package com.wanted.domain.user.entity.location;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import com.wanted.global.config.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 유저의 위치 엔티티
+ */
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "user_location")
+public class UserLocation extends BaseTimeEntity {
+
+  // 유저 위치의 아이디
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  // 위도
+  @Column(name = "lat", nullable = false)
+  private Double lat;
+
+  // 경도
+  @Column(name = "logt", nullable = false)
+  private Double logt;
+}
