d1b85986c3cfec840cce07b81d76bfe3ba118ae5
kawksung <alstjd5479@naver.com>
2023-11-06 23:22:00 +0900

    test: 테스트 및 docs 작성

--- src/main/java/com/wanted/global/config/security/data/TokenReqDto.java
+++ src/main/java/com/wanted/global/config/security/data/TokenReqDto.java
@@ -1,10 +1,14 @@
 package com.wanted.global.config.security.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TokenReqDto {
 
   private String accessToken;
--- src/test/java/com/wanted/domain/auth/api/AuthControllerTest.java
+++ src/test/java/com/wanted/domain/auth/api/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package com.wanted.domain.auth.api;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -8,24 +9,31 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wanted.config.restdocs.AbstractRestDocsTests;
 import com.wanted.domain.auth.application.AuthService;
 import com.wanted.domain.member.MemberTestHelper;
+import com.wanted.domain.member.dto.request.MemberLoginReqDto;
 import com.wanted.domain.member.dto.request.MemberSignUpReqDto;
 import com.wanted.domain.member.entity.Member;
-import com.wanted.global.config.security.SecurityConfig;
+import com.wanted.global.config.security.data.TokenDto;
+import com.wanted.global.config.security.data.TokenReqDto;
+import com.wanted.global.error.BusinessException;
+import com.wanted.global.error.ErrorCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@WebMvcTest(AuthController.class)
-@Import(SecurityConfig.class)
+@WebMvcTest(controllers = AuthController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebMvcConfigurer.class)})
 class AuthControllerTest extends AbstractRestDocsTests {
 
   private static final String AUTH_URL = "/api/v1/auth";
-  
+
   @Autowired
   private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -72,6 +80,113 @@ class AuthControllerTest extends AbstractRestDocsTests {
               .contentType(MediaType.APPLICATION_JSON)
               .content(objectMapper.writeValueAsString(reqDto)))
           .andExpect(status().isBadRequest());
+    }
+  }
+
+  @Nested
+  @DisplayName("로그인 관련 테스트")
+  class login {
+
+    @Test
+    @DisplayName("아이디 비번이 일치할 경우, 토큰이 반환이 성공된다.")
+    void 아이디_비번이_일치할_경우_토큰이_반환이_성공된다_() throws Exception {
+      MemberLoginReqDto reqDto =
+          MemberLoginReqDto.builder()
+              .account("test123")
+              .password("test123*")
+              .build();
+
+      TokenDto tokenDto =
+          TokenDto.builder()
+              .accessToken("새로운 Access 토큰")
+              .refreshToken("새로운 Refresh 토큰")
+              .accessTokenExpiresIn(166574L)
+              .grantType("올바른 Grant 타입")
+              .build();
+
+      given(authService.login((any()))).willReturn(tokenDto);
+
+      mockMvc.perform(post(AUTH_URL + "/login")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(reqDto)))
+          .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("아이디 비번이 일치하지 않을 경우, 토큰이 반환이 실패한다.")
+    void 아이디_비번이_일치하지_않을_경우_토큰이_반환이_실패한다_() throws Exception {
+      MemberLoginReqDto reqDto =
+          MemberLoginReqDto.builder()
+              .account("test123")
+              .password("test123*")
+              .build();
+
+      given(authService.login((any()))).willThrow(
+          new BusinessException(null, "password", ErrorCode.MEMBER_PASSWORD_BAD_REQUEST));
+
+      mockMvc.perform(post(AUTH_URL + "/login")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(reqDto)))
+          .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("올바른 토큰일 경우, 토큰 재발급에 성공한다.")
+    void 올바른_토큰일_경우_토큰_재발급에_성공한다() throws Exception {
+      TokenReqDto reqDto =
+          TokenReqDto.builder()
+              .accessToken("올바른 Access 토큰")
+              .refreshToken("올바른 Refresh 토큰")
+              .build();
+
+      TokenDto tokenDto =
+          TokenDto.builder()
+              .accessToken("새로운 Access 토큰")
+              .refreshToken("새로운 Refresh 토큰")
+              .accessTokenExpiresIn(166574L)
+              .grantType("올바른 Grant 타입")
+              .build();
+
+      given(authService.reissue(any())).willReturn(tokenDto);
+
+      mockMvc.perform(post(AUTH_URL + "/reissue")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(reqDto)))
+          .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 토큰일 경우, 토큰 재발급에 실패한다.")
+    void 올바르지_않은_토큰일_경우_토큰_재발급에_실패한다() throws Exception {
+      TokenReqDto reqDto =
+          TokenReqDto.builder()
+              .accessToken("올바르지 않은 Access 토큰")
+              .refreshToken("올바르지 않은 Refresh 토큰")
+              .build();
+
+      given(authService.reissue(any())).willThrow(
+          new BusinessException(null, "refreshToken", ErrorCode.REFRESH_TOKEN_MISMATCH)
+      );
+
+      mockMvc.perform(post(AUTH_URL + "/reissue")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(reqDto)))
+          .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("올바른 토큰일 경우, 로그아웃에 성공한다.")
+    void 올바른_토큰일_경우_로그아웃에_성공한다() throws Exception {
+      TokenReqDto reqDto =
+          TokenReqDto.builder()
+              .accessToken("올바른 Access 토큰")
+              .refreshToken("올바른 Refresh 토큰")
+              .build();
+
+      mockMvc.perform(post(AUTH_URL + "/logout")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(reqDto)))
+          .andExpect(status().isOk());
     }
   }
 }
